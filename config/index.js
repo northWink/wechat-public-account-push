591dce2a9bfa2c50395c561f0f88ac8963cb6d0c
northWink <62371658+northWink@users.noreply.github.com>
2022-09-06 22:10:40 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -18,11 +18,11 @@ export const config = {
    * [
    *  {
    *    // 想要发送的人的名字
-   *    name: "老婆0",
+   *    name: "五仁月饼",
    *    // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-   *    id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk",
+   *    id: "o2dVB58lSeccsWq24LT1lL5o64SE",
    *    // 你想对他发送的模板消息的模板ID
-   *    useTemplateId: "zjVglIuCXo4CsraXWEio1iP7sZJ7IQEES9lDsgc4yOU",
+   *    useTemplateId: "ux9HKe0e5cz7Ln5sVMYwpCVs_okSeIngAFihPdlrN3Y",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
    *    openUrl: "https://wangxinleo.cn"
    *   }, 
@@ -31,15 +31,15 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "五仁月饼",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "o2dVB58lSeccsWq24LT1lL5o64SE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "ux9HKe0e5cz7Ln5sVMYwpCVs_okSeIngAFihPdlrN3Y",
       // 所在省份
-      province: "广东",
+      province: "安徽",
       // 所在城市
-      city: "肇庆",
+      city: "合肥",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
@@ -80,7 +80,7 @@ export const config = {
   CALLBACK_USERS: [
     {
       // 一般都填自己
-      name: "自己",
+      name: "五仁月饼",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
     }, 
@@ -93,9 +93,9 @@ export const config = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: "广东",
+  PROVINCE: "安徽",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: "惠州",
+  CITY: "合肥",
 
   /** 重要节日相关 */
 
@@ -105,12 +105,12 @@ export const config = {
    * 生日时，name填写想要展示的名字，你可以填“美丽可爱亲亲老婆”
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
-  FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
-  ],
+//   FESTIVALS: [
+//     {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
+//     {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+//     {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+//     {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+//   ],
 
   /**
    * 限制重要节日的展示条目, 需要填写数字; 
@@ -145,14 +145,7 @@ export const config = {
     * */
   SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
-    // 这样配置的话，就会每次随机选一句话发送
-    {"keyword": "lover_prattle", contents: [
-      "因为太喜欢你，所以看谁都像是情敌。",
-      "申请成为你爱里的永久居民。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
-    ]},
+    {"keyword": "moment_copyrighting", contents: ["你主要的问题在于读书太少而想得太多","上班的钱叫工资，上班摸鱼的钱叫挣钱"]},
   ],
 
   /** 每日一言 */
